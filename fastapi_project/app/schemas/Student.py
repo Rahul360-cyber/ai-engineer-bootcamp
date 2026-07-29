c15ef49cecd781814e06ec43a4df8ad44c4ce8fe
Rahul kumar sahoo <rahulsahoo360@gmail.com>
2026-07-29 17:21:56 +0200

    complete advanced FastApi request handling

--- fastapi_project/app/schemas/Student.py
+++ fastapi_project/app/schemas/Student.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter,status
+from fastapi import APIRouter,status,Cookie,Header,Form,File,UploadFile
 from pydantic import BaseModel,Field
+from uuid import  UUID
+from datetime import datetime , time , date
+from typing import Annotated
 
 class address(BaseModel):
       city : str
@@ -11,28 +14,33 @@ class guardian(BaseModel):
       phone : int
 
 class student_info(BaseModel):
-    id : int 
+    student_id :  UUID 
     name : str = Field (min_length = 3,max_length=10 )
     age : int =  Field (gt = 18,lt = 30 , description = "the age should be greater then 18 less then 30 ")
     course : str 
     address : address
     guardian : guardian 
     skills : list[str]
-    
-class studentRefinedInfo (BaseModel):
-       id : int 
+    enrollment_date : datetime 
+    fee : float
+    date_of_birth : date
+
+class student_response (BaseModel):
+       id : UUID 
        name : str 
        course : str 
        age : int
        address : address
        guardian : guardian 
-
+       skills : list[str]
+       enrollment_date : datetime
+       date_of_birth : date
        
 
 router = APIRouter()
 students_n =[]
 
-@router.post("/info",status_code= status.HTTP_201_CREATED,response_model = studentRefinedInfo)
+@router.post("/info",status_code= status.HTTP_201_CREATED,response_model = student_response)
 
 async def info_students(student_info1 : student_info):
     std = student_info1.model_dump()
@@ -42,3 +50,28 @@ async def info_students(student_info1 : student_info):
 @router.get("/students")
 async def nome ():
         return students_n
+
+@router.get("/profile")
+async def sessions(session_id: Annotated[str | None , Cookie()] = None) :
+      if session_id == None:
+            return {"message":"no session_id"}
+
+      return {"session" : session_id}
+
+@router.get("/device")
+async def read_UserAgent(user_agent : Annotated[str | None, Header()]  ):
+      return {"User-Agent":user_agent}
+
+@router.post("/login")
+async def authentication(username : Annotated[str | None, Form()], password : Annotated [str | None,Form()]):
+      return {"message" : "login request received"}
+@router.post("/upload-photo")
+async def file_upload(files : Annotated[list[bytes],File()],files_name : list[UploadFile]):
+      return {"size":len(files),"filename":[i.filename for i in files_name] ,"content_type":[j.content_type for j in files_name]}
+      
+@router.put("/students/{id}")
+async def add_id(id:int):
+      return id
+@router.delete("/students/{id}")
+async def remove_id():
+      return {"message":"successfully done"}
